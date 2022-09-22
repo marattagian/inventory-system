a58c7ce1c3eb0c06e925269b59ccc930ed9b6e5f
Gianni Maratta <marattagian15@gmail.com>
2022-09-22 15:00:39 -0300

    api integration and app life cycle definition

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/labstack/echo/v4"
 	"github.com/marattagian/inventory-system/database"
+	"github.com/marattagian/inventory-system/internal/api"
 	"github.com/marattagian/inventory-system/internal/repository"
 	"github.com/marattagian/inventory-system/internal/service"
 	"github.com/marattagian/inventory-system/settings"
@@ -18,9 +21,25 @@ func main() {
 			database.New,
 			repository.New,
 			service.New,
+			api.New,
+			echo.New,
 		),
-		fx.Invoke(),
+		fx.Invoke(
+			setLifeCycle,
+		),
 	)
 
 	app.Run()
+}
+
+func setLifeCycle(lc fx.Lifecycle, a *api.API, s *settings.Settings, e *echo.Echo)  {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+
+			address := fmt.Sprintf(":%s", s.Port)
+			go a.Start(e, address)
+			
+			return nil
+		},
+	})
 }
