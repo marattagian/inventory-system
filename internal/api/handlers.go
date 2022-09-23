c1eb3e12ae303c24d9f9271b32cda4490509699e
Gianni Maratta <marattagian15@gmail.com>
2022-09-22 22:12:31 -0300

    user login routes and handlers

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/marattagian/inventory-system/encryption"
 	"github.com/marattagian/inventory-system/internal/api/dtos"
 	"github.com/marattagian/inventory-system/internal/service"
 )
@@ -36,4 +38,45 @@ func (a *API) RegisterUser(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusCreated, nil)
+}
+
+func (a *API) LoginUser(c echo.Context) error {
+	ctx := c.Request().Context()
+	params := dtos.LoginUser{}
+
+	err := c.Bind(&params)
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusBadRequest, responseMessage{Message: "invalid request"})
+	}
+
+	err = a.dataValidator.Struct(params)
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusBadRequest, responseMessage{Message: err.Error()})
+	}
+
+	user, err := a.serv.LoginUser(ctx, params.Email, params.Password)
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusBadRequest, responseMessage{Message: err.Error()})
+	}
+
+	token, err := encryption.SignedLoginToken(user)
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusInternalServerError, responseMessage{Message: err.Error()})
+	}
+
+	cookie := &http.Cookie{
+		Name:     "Authorization",
+		Value:    token,
+		Secure:   true,
+		SameSite: http.SameSiteNoneMode,
+		HttpOnly: true,
+	}
+
+	c.SetCookie(cookie)
+
+	return c.JSON(http.StatusOK, map[string]string{"success": "true"})
 }
